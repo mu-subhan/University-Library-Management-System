4de1f655eceaf4921e51a1ded2da1b13af986b03
Muhammad Subhan <muhammadsubhan8934@gmail.com>
2025-01-23 18:16:57 +0500

    home data

---
+++ app/(root)/page.tsx
@@ -0,0 +1,19 @@
+import BookList from "@/components/BookList";
+import BookOverview from "@/components/BookOverview";
+import { sampleBooks } from "@/constants";
+
+
+
+const Home =() =>{
+  return (
+  <>
+  <BookOverview {...sampleBooks[0]}/>
+  <BookList
+  title="Latest Book"
+  books = {sampleBooks}
+  containerClassName = "mt-28"
+  />
+  </>
+  );
+}
+export default Home
--- components/BookList.tsx
+++ components/BookList.tsx
@@ -1,9 +1,23 @@
 import React from 'react'
+import BookCard from './BookCard';
+interface Props{
+  title:string;
+  books:Book[];
+  containerClassName?:string;
+}
 
-const  BookList = () => {
+const BookList = ({title,books,containerClassName} : Props) => {
   return (
-    <section className='font-bebas-neue text-4xl text-light-100'>
-      Popular Books
+    <section className={containerClassName}>
+    <h2 className='font-bebas-neue text-4xl text-light-100'>
+     {title}
+    </h2>
+
+    <ul className='book-list'>
+      {books.map((book) =>(
+        <BookCard key={book.title} {...book} />
+      ))}
+    </ul>
     </section>
   )
 }
